df824658cf49692ed44b6717df22a064b3213e20
oski1306 <oski1306@o2.pl>
2023-03-22 15:05:26 +0100

    commit

--- public/script/templates/About.js
+++ public/script/templates/About.js
@@ -7,8 +7,16 @@ export default class extends AbstractView{
     }
     async getPage(){
        return `
-       <h2 class="title3" translateKey="About">About Me</h2>
-       <p translateKey="AboutText">About Me</p>
+       <h2 class="title3" translateKey="About">Choose Language to show text</h2>
+       <div class="myPic" >
+          <img src="https://cdn.discordapp.com/attachments/358644695360077834/1088079861421191188/Meg.jpg" width="200px" height="200px">
+       </div>
+       <p class="aboutTxt" translateKey="AboutText1"></p>
+       <p class="aboutTxt" translateKey="AboutText2"></p>
+       <p class="aboutTxt" translateKey="AboutText3"></p>
+       <p class="aboutTxt" translateKey="AboutText4"></p>
+       <p class="aboutTxt" translateKey="AboutText5"></p> 
+       <p class="aboutTxt" translateKey="AboutText6"></p> 
    `;
     }
 }
