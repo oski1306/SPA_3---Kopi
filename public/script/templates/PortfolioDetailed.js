7e07b0bcfb4e8bb2d8f65131d1c39bd881bcc8ba
oski1306 <oski1306@o2.pl>
2023-03-22 16:38:37 +0100

    commit

--- public/script/templates/PortfolioDetailed.js
+++ public/script/templates/PortfolioDetailed.js
@@ -8,7 +8,18 @@ export default class extends AbstractView{
     async getPage(){
        return `
        <h2 class="title2">OD Models</h2>
-       <p translateKey="PortfolioText">askljhdljasl</p>
+       <div class="odPics">
+            <img src="https://media.discordapp.net/attachments/358644695360077834/1088110759856308385/OD_1.PNG" width="400px" height="200px">
+            <img src="https://media.discordapp.net/attachments/358644695360077834/1088110783336034375/OD_2.PNG" width="200px" height="200px">
+            <img src="https://cdn.discordapp.com/attachments/358644695360077834/1088110808355053618/OD_3.PNG" width="200px" height="200px">
+            <img src="https://cdn.discordapp.com/attachments/358644695360077834/1088111649715650661/OD_4.PNG" width="200px" height="200px">
+       </div>
+       <p class="odText" translateKey="odText1"></p>
+       <p class="odText" translateKey="odText2"></p>
+       <p class="odText" translateKey="odText3"></p>
+       <p class="odText" translateKey="odText4"></p>
+       <p class="odText" translateKey="odText5"></p>
+       <p class="odText" translateKey="odText6"></p>
    `;
     }
 }
