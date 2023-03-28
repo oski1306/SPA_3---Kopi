73929dd04a381e8240bdb84b718a1d23bcb24973
oski1306 <oski1306@o2.pl>
2023-03-28 15:18:10 +0200

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = express();
 const path = require('path');
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 server.use('/script', express.static(path.resolve(__dirname,'public', 'script')));
 
