fd9f8936193189b7bec854cbada20978eef13046
oski1306 <oski1306@o2.pl>
2023-03-28 15:38:20 +0200

    commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const server = express();
 const path = require('path');
 const port = process.env.PORT || 8080;
+const host = process.env.HOST || '0.0.0.0';
 
 server.use('/script', express.static(path.resolve(__dirname,'public', 'script')));
 
@@ -10,3 +11,7 @@ server.get('/*', (req,res)=>{
 });
 
 console.log('Server running in port:' + port);
+
+server.listen(port, host, function{
+    console.log("Server is running in port:" + port);
+});
